40cd74f3c605afa33321cf420f6069cde31b6ec3
stevenmarp <stevenmarp@sinerka.id>
2026-01-19 17:33:07 +0700

    Migrate to Odoo 19.0

--- sm_recycle_bin_sales_order/__manifest__.py
+++ sm_recycle_bin_sales_order/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Recycle Bin - Sales Order',
-    'version': '18.0.1.0.0',
+    'version': '19.0.1.0.0',
     'category': 'Sales',
     'summary': 'Track deleted Sales Orders with user info, IP address, and deletion details',
     'description': '''
